2c5a178d3c38154f9e7703030284be79736f7d05
w1am <william@inorbit.com>
2022-03-30 22:18:05 +0400

    242: compare sorted bytes

--- problems/242-valid-anagram/2420-valid-anagram.go
+++ problems/242-valid-anagram/2420-valid-anagram.go
@@ -1,44 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"reflect"
+	"sort"
 )
 
 func isAnagram(s string, t string) bool {
-	// Check if length is same
-	var match bool = len(s) == len(t)
+	sb, tb := []byte(s), []byte(t)
 
-	if !match {
-		return false
-	}
+	sort.Slice(sb, func(i int, j int) bool { return sb[i] < sb[j] })
+	sort.Slice(tb, func(i int, j int) bool { return tb[i] < tb[j] })
 
-	table_s := make(map[string]int)
-	table_t := make(map[string]int)
-
-	for i := 0; i < len(s); i++ {
-		var current string = string(s[i])
-		_, isPresent := table_s[current]
-		if isPresent {
-			table_s[current] = table_s[current] + 1
-		} else {
-			table_s[current] = 1
-		}
-	}
-
-	for i := 0; i < len(s); i++ {
-		var current string = string(t[i])
-		_, isPresent := table_t[current]
-		if isPresent {
-			table_t[current] = table_t[current] + 1
-		} else {
-			table_t[current] = 1
-		}
-	}
-
-	eq := reflect.DeepEqual(table_s, table_t)
-
-	return eq
+	return bytes.Equal(sb, tb)
 }
 
 func main() {
